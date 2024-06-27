c1f8d256238f23754b25d259068c331e78549134
Tsung Ti Lee <tsungbol90906@gmail.com>
2024-06-27 00:43:00 -0400

    Update rc_cars.js

--- Content/rc_cars.js
+++ Content/rc_cars.js
@@ -2,22 +2,77 @@ const rcCarMilestones = [
     {
         title: "Components",
         document: [
-            "bullet points",
-            "goes",
-            "here"
+            "DC Motor, Servo Motor, ESC (Electronic Speed Controller), Battery, Receiver, Transmitter (remote control)",
+            "Chassis, Wheels (the round part) and Tires (the rubber part), Suspension System, Drivetrain (2WD, 4WD), Body Shell, Differential (control wheels in different speed)",
         ],
         pictures: [
             { src: "", alt: "" }
         ]
     },
     {
-        title: "Headphones Hanger",
+        title: "Motors",
         document: [
-            "Inspired by: Not enough space on the desk",
-            "I wanted to save space"
+            "2 Main Types: Brushed Motor, Brushless Motor",
+            "Brushed Motor: Generally cheaper and easier to maintain",
+            "Brushless Motor: More efficient and powerful but more expensive",
         ],
         pictures: [
             { src: "", alt: "" }
         ]
-    }
+    },
+    {
+        title: "ESC (Electronic Speed Controller)",
+        document: [
+            "2 Main Types: Brushed Motor, Brushless Motor",
+            "Brushed Motor: Generally cheaper and easier to maintain",
+            "Brushless Motor: More efficient and powerful but more expensive",
+        ],
+        pictures: [
+            { src: "", alt: "" }
+        ]
+    },
+    {
+        title: "Battery",
+        document: [
+            "2 Main Types: Brushed Motor, Brushless Motor",
+            "Brushed Motor: Generally cheaper and easier to maintain",
+            "Brushless Motor: More efficient and powerful but more expensive",
+        ],
+        pictures: [
+            { src: "", alt: "" }
+        ]
+    },
+    {
+        title: "Receiver and Transmitter",
+        document: [
+            "2 Main Types: Brushed Motor, Brushless Motor",
+            "Brushed Motor: Generally cheaper and easier to maintain",
+            "Brushless Motor: More efficient and powerful but more expensive",
+        ],
+        pictures: [
+            { src: "", alt: "" }
+        ]
+    },
+    {
+        title: "Wheels and Tires",
+        document: [
+            "2 Main Types: Brushed Motor, Brushless Motor",
+            "Brushed Motor: Generally cheaper and easier to maintain",
+            "Brushless Motor: More efficient and powerful but more expensive",
+        ],
+        pictures: [
+            { src: "", alt: "" }
+        ]
+    },
+    {
+        title: "DC Motor",
+        document: [
+            "2 Main Types: Brushed Motor, Brushless Motor",
+            "Brushed Motor: Generally cheaper and easier to maintain",
+            "Brushless Motor: More efficient and powerful but more expensive",
+        ],
+        pictures: [
+            { src: "", alt: "" }
+        ]
+    },
 ];
